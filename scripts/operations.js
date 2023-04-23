ce8ce4dcb7bc7515c5af1ce235352e126b54ef56
k-fields <caiocp3@hotmail.com>
2023-04-23 04:00:06 -0300

    Adds operations file that exports all calc functions.

---
+++ scripts/operations.js
@@ -0,0 +1,19 @@
+const operations = {
+    'multiply': () => {
+        console.log("Trigger multiply")
+    },
+    'subtract': () => {
+        console.log("Trigger subtract");
+    },
+    'add': () => {
+        console.log("Trigger add");
+    },
+    'divide': () => {
+        console.log("Trigger divide");
+    },
+    'equals': () => {
+        console.log("Trigger equals");
+    }
+}
+
+export default operations;
