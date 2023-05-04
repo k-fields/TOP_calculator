c37ff87d4309bc71150e1c409d921587759255c2
k-fields <caiocp3@hotmail.com>
2023-05-04 02:50:43 -0300

    Implements all the operations.

--- scripts/operations.js
+++ scripts/operations.js
@@ -3,30 +3,34 @@ export const operations = {
         if (!elements || elements.length == 1) return;
         
         return elements.reduce((acc, element) => acc *= element);
-        console.log("Trigger multiply: " + elements)
     },
-    'subtract': () => {
-        console.log("Trigger subtract");
-    },
-    'add': () => {
-        console.log("Trigger add");
-    },
-    'divide': () => {
-        console.log("Trigger divide");
-    },
-    'equals': () => {
-        console.log("Trigger equals");
-    },
-}
+    'subtract': (elements) => {
+        if (!elements || elements.length == 1) return;
 
-export function handler(buffer){
-    if (buffer.length > 2) {
+        return elements.reduce((acc, element) => acc -= element);
+    },
+    'add': (elements) => {
+        if (!elements || elements.length == 1) return;
+
+        return elements.reduce((acc, element) => acc += element);
+    },
+    'divide': (elements) => {
+        if (!elements || elements.length == 1 || elements[1] == 0) return;
+        
+        return elements.reduce((acc, element) => acc /= element);
+    },
+    'equals': (buffer) => {
+        if (buffer.length > 2) {
             
-        if (operations[buffer[0]] === undefined) return;
-
-        operations[buffer[0]](buffer.slice(1));
-
-        }
+            if (operations[buffer[0]] === undefined) return;
+    
+            operations[buffer[0]](buffer.slice(1));
+    
+            }
+    },
 }
 
 console.log(operations.multiply([2,3,5]));
+console.log(`Subtract 4 - 5: ${operations.subtract([4,5])}`);
+console.log(`Add 5+2+3: ${operations.add([5,2,3])}`);
+console.log(`Divide 5/2: ${operations.divide([5,2])}`);
