1d9a9ed5f6425ed8452e46ece8df0497768623ac
k-fields <caiocp3@hotmail.com>
2023-05-04 15:16:28 -0300

    Adds constants to facilitate code readability.

--- scripts/display.js
+++ scripts/display.js
@@ -5,8 +5,6 @@ const displayCurrent = document.getElementsByClassName('content')[0];
 const displayCurrentStyles = getComputedStyle(displayCurrent);
 const displayCurrentParentStyles = getComputedStyle(displayCurrent.parentNode);
 
-let buffer = [];
-
 document.addEventListener('keydown', (e) => {
     handle(e.key);        
 })
@@ -16,10 +14,30 @@ document.addEventListener('click', (e) => {
 });
 
 function handle (key){
-    if (/[0-9\.]/.test(parseInt(key)) && 
-        parseInt(displayCurrentStyles['width']) < parseInt(displayCurrentParentStyles['width']))
-        {
-            displayCurrent.textContent += parseInt(key);
+
+    const CURRENT_WIDTH = displayCurrentStyles['width'];
+    const CURRENT_PARENT_WIDTH = displayCurrentParentStyles['width'];
+    const NUMBER = /^[0-9\.]$/.test(key);
+    const OPERATION = /^[*\/\-+]{1}$/.test(key);
+    const BUFFERPATTERN = /^[0-9]*(\.{1}[0-9]+)?[+\-\/*]{1}[0-9]*(\.{1}[0-9]+)?$/g
+
+    
+        //regular number or '.' button was pressed 
+        if (CURRENT_WIDTH < CURRENT_PARENT_WIDTH){
+            if (NUMBER){
+                displayCurrent.textContent += key;
+            }
+
+            if(OPERATION){
+                displayCurrent.textContent += key;
+            }
+
+            console.log("Text: " + displayCurrentStyles.width +" | Display: "+displayCurrentParentStyles.width+" | Key: "+ key);
         }
-        console.log("Text: " + displayCurrentStyles.width +" | Display: "+displayCurrentParentStyles.width+" | Key: "+ key);
+
+        if (BUFFERPATTERN.test(key)){
+            console.log(`Pattern test - Key: ${key} - ${BUFFERPATTERN.test(key)}`);
+            operations['operationHandler'](key);
+        }
 }
+
