36b16279dc6d2384ad160df443283d7c1666b5c7
k-fields <caiocp3@hotmail.com>
2023-05-04 11:51:18 -0300

    Parses integers before adding them to the display

--- scripts/display.js
+++ scripts/display.js
@@ -1,13 +1,5 @@
 import {operations} from './operations.js'
 
-const execute = {
-    '*':operations.multiply,
-    '+':operations.add,
-    '-':operations.subtract,
-    '/':operations.divide,
-    '=':operations.equals,
-    'Enter':operations.equals
-}
 
 const displayCurrent = document.getElementsByClassName('content')[0];
 const displayCurrentStyles = getComputedStyle(displayCurrent);
@@ -16,28 +8,16 @@ const displayCurrentParentStyles = getComputedStyle(displayCurrent.parentNode);
 let buffer = [];
 
 document.addEventListener('keydown', (e) => {
-    if (!(execute[e.key] !== undefined)){ //if key pressed is not tied to any function
-        if (/[0-9\.]/.test(e.key) && 
+    
+        if (/[0-9\.]/.test(parseInt(e.key)) && 
         parseInt(displayCurrentStyles['width']) < parseInt(displayCurrentParentStyles['width']))
         {
-            displayCurrent.textContent += e.key;
+            displayCurrent.textContent += parseInt(e.key);
         }
         console.log("Text: " + displayCurrentStyles.width+" | Display: "+displayCurrentParentStyles.width+" | Key: "+e.key);
         
-    }
-    else
-    {
-        if (displayCurrent.textContent && !buffer){
-            buffer[1] = displayCurrent.textContent;
-            buffer[0] = e.key;
-            displayCurrent.textContent += e.key;
-        }
-        else if (displayCurrent.textContent && buffer){
-            buffer[2] = 
-        }
-        equals(buffer)
-    }
 })
 
-
-
+document.addEventListener('click', (e) => {
+   // execute['handler'];
+});
